099d1be21292686c4c754ff0618203c481412457
suppakorn <suppakorn.resume@gmail.com>
2022-05-18 01:23:53 +0700

    feat: define routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,24 @@
 import { Express, Request, Response } from "express"
+import log from "./logger"
+import { placeBid } from "./service/market.service"
 
 export default function (app: Express) {
-    app.get("/market/healthcheck", (req: Request, res: Response) => {
+    app.get("/api/healthcheck", (req: Request, res: Response) => {
         res.sendStatus(200)
     })
+
+    app.get("/api/market/ticker", (req: Request, res: Response) => {
+        res.sendStatus(200)
+    })
+
+    app.post("/api/market/place-bid/test", (req: Request, res: Response) => {
+        placeBid(req.body)
+            .then((data) => {
+                res.send(data)
+                res.sendStatus(200)
+            }).catch((err) => {
+                res.send(500)
+            })
+    })
+
 }
